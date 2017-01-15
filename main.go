a89775ecea5aab97fa2469d2f0eaefa096f13c4b
Christian Kniep <christian@qnib.org>
2017-01-15 09:40:35 +0100

    Bump version: 1.1.1 → 1.2.0

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "go-testhttpd"
-	app.Version = "1.1.1"
+	app.Version = "1.2.0"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "status-sequence",
